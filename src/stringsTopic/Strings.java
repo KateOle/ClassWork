ebac2b31900f7c8cfab7e0f24c0fd8334f07a253
KateOle <kateolieinik@gmail.com>
2024-11-13 19:34:25 +0100

    Strings - 329, 330, 331, 332, 333

--- src/stringsTopic/Strings.java
+++ src/stringsTopic/Strings.java
@@ -1,14 +1,13 @@
 package stringsTopic;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Strings {
 
     public static void main(String[] args) {
-       lastWordInSentence();
-
+        whiteSpaces();
     }
 
     public static void stringUpperLowerCases() {
@@ -347,7 +346,7 @@ public class Strings {
 
         String[] arrayOfWords = sent.split(" ");
 
-        System.out.println(arrayOfWords[arrayOfWords.length-1]);
+        System.out.println(arrayOfWords[arrayOfWords.length - 1]);
     }
 
     public static void penguins() {
@@ -362,8 +361,223 @@ public class Strings {
 //               /  V  \    /  V  \    /  V  \    /  V  \
 //              /(  1  )\  /(  2  )\  /(  3  )\  /(  4  )\
 //                ^^ ^^      ^^ ^^      ^^ ^^      ^^ ^^
+        Scanner scn = new Scanner(System.in);
 
+        System.out.println("Enter number of pinguins from 1 to 9");
+
+        int n = scn.nextInt();
+
+        String str01 = "   _~_   " + "  ";
+        String str02 = "  (o o)  " + "  ";
+        String str03 = " /  V  \\ " + "  ";
+        String str04 = "/(  %  )\\" + "  ";
+        String str05 = "  ^^ ^^  " + "  ";
+        String str1 = "";
+        String str2 = "";
+        String str3 = "";
+        String str4 = "";
+        String str5 = "";
+
+        for (int i = 0; i < n; i++) {
+            str1 += str01;
+            str2 += str02;
+            str3 += str03;
+            str4 += str04.replace("%", String.valueOf(i + 1));
+            str5 += str05;
+
+        }
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(str5);
 
     }
- }
+
+    public static void numbersFromString() {
+//    335. Дано рядок, який, можливо, містить пропуски. «Витягніть» з цього рядка всі символи, які є цифрами і складіть
+//    з них новий рядок.
+//    3+3=6     -- 336
+//    2 * 3 = 6   --- 236
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter some phrase");
+
+        String row = scn.nextLine();
+
+        Character stor;
+
+
+        for (int i = 0; i < row.length(); i++) {
+            stor = row.charAt(i);
+            if (stor >= 48 && stor <= 57) {
+                System.out.print(stor + " ");
+            }
+        }
+    }
+
+    public static void accessToAccount() {
+//    336. Для доступу до власного акаунту на сайті соціальної мережі користувач ввів логін і
+//    пароль. Так як була увімкнена двофакторна авторизація, на його телефон прийшло повідомлення
+//    з рядком цифр та інформацією як отримати код доступу. У повідомленні йшлося: «Кожну цифру, яка більша 5,
+//    необхідно націло розділити на 2, а потім з утвореної послідовності цифр видалити усі парні числа». Який код
+//    повинен ввести користувач для успішної авторизації? Напишіть програму, на вхід якої вводиться рядок цифр із
+//        повідомлення, а програма повинна надрукувати правильний код доступу.
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter some code");
+
+        String code = scn.nextLine();
+
+        int count = 0;
+        for (int i = 0; i < code.length(); i++) {
+            Character codeChar = code.charAt(i);
+            if (codeChar > 53 && codeChar < 58) {
+                count = (codeChar - 48) / 2;
+                if (count % 2 != 0) {
+                    System.out.print(count + " ");
+                }
+            }
+        }
+    }
+
+    public static void asciiFromTo() {
+//    337. Виведіть усі символи ASCII з кодами від n (n > 32) до m (m < 127) і їх коди в наступному вигляді: «символ код».
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter star code");
+
+        int fromChar = scn.nextInt();
+
+        System.out.println("Enter end code");
+
+        int toChar = scn.nextInt();
+
+        for (int i = fromChar; i <= toChar; i++) {
+            char result = (char) i;
+            System.out.printf("%c %d \n", result, i);
+        }
+    }
+
+    public static void removeSpacesFromTheSent() {
+//  338.  У рядку є кілька слів, розділених одним або декількома пропусками. Потрібно прибрати з тексту зайві пропуски:
+//    два і більше пропусків поспіль, а також всі пропуски на початку і в кінці рядка. На вхід програмі подається рядок,
+//    що складається не більше ніж з 255 символів. Надрукувати новий рядок.
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter some sentence");
+
+        String line = scn.nextLine();
+
+        String resultLine = line.replaceAll("\\s+", " ").trim();
+
+        System.out.println(resultLine);
+    }
+
+    public static void calculationFromTheString() {
+//    339. Дано вираз, який має один з наступних виглядів: 'A+B', 'A-B' або 'A*B', де A і B - цілі числа від 0 до 1000000000.
+//    Визначте значення цього виразу.
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter some statememnt");
+
+        String statem = scn.nextLine();
+
+        boolean plus = statem.contains("+");
+        boolean minus = statem.contains("-");
+        boolean multip = statem.contains("*");
+
+        String[] numberFromTheStringBasic = null;
+        int numbRes = 0;
+
+        if (plus) {
+            numberFromTheStringBasic = statem.split("\\+");
+            int numb = Integer.parseInt(numberFromTheStringBasic[0]);
+            int numb2 = Integer.parseInt(numberFromTheStringBasic[1]);
+            numbRes = numb + numb2;
+
+        } else if (minus) {
+            numberFromTheStringBasic = statem.split("\\-");
+            int numbMin1 = Integer.parseInt(numberFromTheStringBasic[0]);
+            int numbMin2 = Integer.parseInt(numberFromTheStringBasic[1]);
+            numbRes = numbMin1 - numbMin2;
+
+        } else if (multip) {
+            numberFromTheStringBasic = statem.split("\\*");
+            int numbMult1 = Integer.parseInt(numberFromTheStringBasic[0]);
+            int numMult2 = Integer.parseInt(numberFromTheStringBasic[1]);
+            numbRes = numbMult1 * numMult2;
+        }
+        System.out.println(numbRes);
+    }
+
+    public static void stringToLowerCase() {
+//    340. Дано рядок, який, можливо, містить пропуски. Переведіть усі символи цього рядка в нижній регістр.
+//    Hearts of Three, by Jack London
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter a line");
+
+        String statem = scn.nextLine();
+
+        String lowerCaseLine = statem.toLowerCase();
+
+        System.out.println(lowerCaseLine);
+
+    }
+
+    public static void lineOfAsciiCodes() {
+//    341. Виведіть поспіль, без пропусків, усі символи, що лежать в таблиці ASCII між двома заданими символами.
+//    Програма отримує на вхід два символу, кожен в окремому рядку і повинна вивести рядок, що починається першим
+//    із заданих символів і закінчується другим.
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter first character");
+
+        char asciiChar1 = scn.next().charAt(0);
+
+        System.out.println("Enter second character");
+
+        char asciiChar2 = scn.next().charAt(0);
+
+        int asciiInt1 = asciiChar1;
+        int asciiInt2 = asciiChar2;
+
+        for (int i = asciiInt1; i <= asciiInt2; i++) {
+            System.out.print((char) i);
+        }
+    }
+
+    public static void whiteSpaces() {
+//    343. Дано рядок. Визначити число пропусків в ньому.
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter some line");
+
+        String line = scn.nextLine();
+
+        int count = line.length() - line.replace(" ", "").length();
+
+        System.out.println(count);
+    }
+
+    public static void whiteSpaces1() {
+//    343. Дано рядок. Визначити число пропусків в ньому.
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter some line");
+
+        String line = scn.nextLine();
+
+
+        // StringUtils - podkluczit
+    }
+}
 
