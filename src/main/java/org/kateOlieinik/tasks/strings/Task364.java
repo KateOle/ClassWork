d9227d6d41528f65fd819352094315c8e4fbaaa6
KateOle <kateolieinik@gmail.com>
2024-12-17 00:29:43 +0100

    Tasks 364 Linkedlist, String

---
+++ src/main/java/org/kateOlieinik/tasks/strings/Task364.java
@@ -0,0 +1,36 @@
+package org.kateOlieinik.tasks.strings;
+
+import java.util.*;
+
+public class Task364 {
+    //364. Вводиться рядок. Потрібно видалити з нього повторювані символи і всі пропуски.
+//    aa
+//    a a b b c dd e
+
+    public static void main(String[] args) {
+        removeSpacesAndDoubleLettersFromRow();
+    }
+
+    public static void removeSpacesAndDoubleLettersFromRow() {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter some row");
+
+        String row = scn.nextLine();
+
+        String withoutSpaces = row.replace(" ", "");
+        char[] withoutSpacesArray = withoutSpaces.toCharArray();
+
+        List<Character> newList = new LinkedList<Character>();
+
+        for (int i = 0; i < withoutSpacesArray.length; i++) {
+            if (newList.contains(withoutSpacesArray[i])) {
+            } else newList.add(withoutSpacesArray[i]);
+        }
+
+        for (int i = 0; i < newList.size(); i++) {
+            System.out.print(newList.get(i));
+        }
+    }
+}
